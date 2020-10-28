dc7a4664812c0b826c939a3d6481c6c79108c575
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2020-10-27 21:52:51 -0400

    fixes

--- provision-with-installer-from-s3/resources/download_helper.sh
+++ provision-with-installer-from-s3/resources/download_helper.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-#############################################################################################
-######## generic downlaad
-#############################################################################################
+#set the PATH to make sure the AWS CLI tool is found
+PATH=$PATH:/usr/local/bin/
 
 function download_artifacts { 
     if [ "x$REMOTE_S3_BUCKET" == "x" ]; then
@@ -19,20 +18,24 @@ function download_artifacts {
     echo "Download artifacts from S3 with S3_BASE_URL=$S3_BASE_URL"
 
     ## installers
-    if [ ! -f $LOCAL_PRODUCT_INSTALLER ]; then
+    if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_PRODUCT_INSTALLER ]; then
+        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_INSTALLER}"
         aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_INSTALLER} $LOCAL_PRODUCT_INSTALLER
     else
         echo "$LOCAL_PRODUCT_INSTALLER already downloaded...not downloading again."
     fi
 
-    if [ ! -f $LOCAL_PRODUCTUPDATES_INSTALLER ]; then
+    if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_PRODUCTUPDATES_INSTALLER ]; then
+        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCTUPDATES_INSTALLER}"
         aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCTUPDATES_INSTALLER} $LOCAL_PRODUCTUPDATES_INSTALLER
     else
         echo "$LOCAL_PRODUCTUPDATES_INSTALLER already downloaded...not downloading again."
     fi
 
     ## licenses
-    if [ ! -f $LOCAL_LICENSE_ZIP ]; then
+    if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_LICENSE_ZIP ]; then
+        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_LICENSES}"
+        
         aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_LICENSES} $LOCAL_LICENSE_ZIP
         mkdir -p $LOCAL_TARGET_DIR/licenses/
         unzip $LOCAL_LICENSE_ZIP -d $LOCAL_TARGET_DIR/licenses/
@@ -43,26 +46,30 @@ function download_artifacts {
     fi
 
     ## install product image / script
-    if [ ! -f $LOCAL_PRODUCT_SCRIPT ]; then
+    if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_PRODUCT_SCRIPT ]; then
+        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_SCRIPT}"
         aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_SCRIPT} $LOCAL_PRODUCT_SCRIPT
     else
         echo "$LOCAL_PRODUCT_SCRIPT already downloaded...not downloading again."
     fi
 
-    if [ ! -f $LOCAL_PRODUCT_IMAGE ]; then
+    if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_PRODUCT_IMAGE ]; then
+        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_IMAGE}"
         aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_IMAGE} $LOCAL_PRODUCT_IMAGE
     else
         echo "$LOCAL_PRODUCT_IMAGE already downloaded...not downloading again."
     fi
 
     ## install fix image / script
-    if [ ! -f $LOCAL_FIX_SCRIPT ]; then
+    if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_FIX_SCRIPT ]; then
+        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_SCRIPT}"
         aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_SCRIPT} $LOCAL_FIX_SCRIPT
     else
         echo "$LOCAL_FIX_SCRIPT already downloaded...not downloading again."
     fi
 
-    if [ ! -f $LOCAL_FIX_IMAGE ]; then
+    if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_FIX_IMAGE ]; then
+        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_IMAGE}"
         aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_IMAGE} $LOCAL_FIX_IMAGE
     else
         echo "$LOCAL_FIX_IMAGE already downloaded...not downloading again."
