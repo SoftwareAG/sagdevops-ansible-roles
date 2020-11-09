0d3e38790999f50d942264d7856abd44f5e84d0e
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2020-11-09 14:32:22 -0500

    updating installation scripts

--- provision-with-installer-from-s3/resources/download_helper.sh
+++ provision-with-installer-from-s3/resources/download_helper.sh
@@ -19,58 +19,85 @@ function download_artifacts {
 
     ## installers
     if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_PRODUCT_INSTALLER ]; then
-        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_INSTALLER}"
-        aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_INSTALLER} $LOCAL_PRODUCT_INSTALLER
+        if [ "x${REMOTE_S3_BUCKET_PRODUCT_INSTALLER}" != "x" ]; then
+            echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_INSTALLER}"
+            aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_INSTALLER} $LOCAL_PRODUCT_INSTALLER
+        else
+            echo "[WARN] Variable REMOTE_S3_BUCKET_PRODUCT_INSTALLER is not defined or empty...will not download."
+        fi
     else
         echo "$LOCAL_PRODUCT_INSTALLER already downloaded...not downloading again."
     fi
 
     if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_PRODUCTUPDATES_INSTALLER ]; then
-        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCTUPDATES_INSTALLER}"
-        aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCTUPDATES_INSTALLER} $LOCAL_PRODUCTUPDATES_INSTALLER
+        if [ "x${REMOTE_S3_BUCKET_PRODUCTUPDATES_INSTALLER}" != "x" ]; then
+            echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCTUPDATES_INSTALLER}"
+            aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCTUPDATES_INSTALLER} $LOCAL_PRODUCTUPDATES_INSTALLER            
+        else
+            echo "[WARN] Variable REMOTE_S3_BUCKET_PRODUCTUPDATES_INSTALLER is not defined or empty...will not download."
+        fi
     else
         echo "$LOCAL_PRODUCTUPDATES_INSTALLER already downloaded...not downloading again."
     fi
 
     ## licenses
     if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_LICENSE_ZIP ]; then
-        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_LICENSES}"
-        
-        aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_LICENSES} $LOCAL_LICENSE_ZIP
-        mkdir -p $LOCAL_TARGET_DIR/licenses/
-        unzip $LOCAL_LICENSE_ZIP -d $LOCAL_TARGET_DIR/licenses/
+        if [ "x${REMOTE_S3_BUCKET_LICENSES}" != "x" ]; then
+            echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_LICENSES}"
+            aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_LICENSES} $LOCAL_LICENSE_ZIP
 
-        cp "$LOCAL_TARGET_DIR/licenses/$REMOTE_S3_BUCKET_INZIP_LICENSE_PATH" $LOCAL_LICENSE_PATH
+            mkdir -p $LOCAL_TARGET_DIR/licenses/
+            unzip $LOCAL_LICENSE_ZIP -d $LOCAL_TARGET_DIR/licenses/
+            cp "$LOCAL_TARGET_DIR/licenses/$REMOTE_S3_BUCKET_INZIP_LICENSE_PATH" $LOCAL_LICENSE_PATH
+        else
+            echo "[WARN] Variable REMOTE_S3_BUCKET_LICENSES is not defined or empty...will not download."
+        fi
     else
         echo "$LOCAL_LICENSE_ZIP already downloaded...not downloading again."
     fi
 
     ## install product image / script
     if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_PRODUCT_SCRIPT ]; then
-        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_SCRIPT}"
-        aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_SCRIPT} $LOCAL_PRODUCT_SCRIPT
+        if [ "x${REMOTE_S3_BUCKET_PRODUCT_SCRIPT}" != "x" ]; then
+            echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_SCRIPT}"
+            aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_SCRIPT} $LOCAL_PRODUCT_SCRIPT            
+        else
+            echo "[WARN] Variable REMOTE_S3_BUCKET_PRODUCT_SCRIPT is not defined or empty...will not download."
+        fi
     else
         echo "$LOCAL_PRODUCT_SCRIPT already downloaded...not downloading again."
     fi
 
     if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_PRODUCT_IMAGE ]; then
-        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_IMAGE}"
-        aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_IMAGE} $LOCAL_PRODUCT_IMAGE
+        if [ "x${REMOTE_S3_BUCKET_PRODUCT_IMAGE}" != "x" ]; then
+            echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_IMAGE}"
+            aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_PRODUCT_IMAGE} $LOCAL_PRODUCT_IMAGE            
+        else
+            echo "[WARN] Variable REMOTE_S3_BUCKET_PRODUCT_IMAGE is not defined or empty...will not download."
+        fi
     else
         echo "$LOCAL_PRODUCT_IMAGE already downloaded...not downloading again."
     fi
 
     ## install fix image / script
     if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_FIX_SCRIPT ]; then
-        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_SCRIPT}"
-        aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_SCRIPT} $LOCAL_FIX_SCRIPT
+        if [ "x${REMOTE_S3_BUCKET_FIX_SCRIPT}" != "x" ]; then
+            echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_SCRIPT}"
+            aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_SCRIPT} $LOCAL_FIX_SCRIPT
+        else
+            echo "[WARN] Variable REMOTE_S3_BUCKET_FIX_SCRIPT is not defined or empty...will not download."
+        fi
     else
         echo "$LOCAL_FIX_SCRIPT already downloaded...not downloading again."
     fi
 
     if [ "$FORCE_REDOWNLOAD" == "true" ] || [ ! -f $LOCAL_FIX_IMAGE ]; then
-        echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_IMAGE}"
-        aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_IMAGE} $LOCAL_FIX_IMAGE
+        if [ "x${REMOTE_S3_BUCKET_FIX_IMAGE}" != "x" ]; then
+            echo "Downloading file at ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_IMAGE}"
+            aws s3 cp ${S3_BASE_URL}/${REMOTE_S3_BUCKET_FIX_IMAGE} $LOCAL_FIX_IMAGE
+        else
+            echo "[WARN] Variable REMOTE_S3_BUCKET_FIX_IMAGE is not defined or empty...will not download."
+        fi
     else
         echo "$LOCAL_FIX_IMAGE already downloaded...not downloading again."
     fi
