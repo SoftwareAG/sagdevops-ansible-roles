b43295aa5197a0954c45699d12c5b119ac88445a
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2021-02-22 00:32:30 -0500

    update

--- configure-integrationserver/templates/reset_node.sh
+++ configure-integrationserver/templates/reset_node.sh
@@ -17,16 +17,5 @@ rm -Rf ${SAG_HOME}/profiles/IS_${INSTANCE_NAME}/logs/*
 rm -Rf ${SAG_HOME}/profiles/SPM/logs/*
 rm -Rf ${SAG_HOME}/install/logs/*
 
-##./is_instance.sh delete-osgi-profile -Dinstance.name=${INSTANCE_NAME}; 
-cd ${SAG_HOME}/IntegrationServer/instances; ./is_instance.sh create-osgi-profile -Dinstance.name=${INSTANCE_NAME}
-
-# install/etc/installconfig.prop:hostname=ip-172-40-192-143
-# IntegrationServer/instances/default/packages/WmAssetPublisher/config/assetpublisher.cnf:    <value name="is_hostname">ip-172-40-192-143.ad.clouddemo.saggov.local</value>
-# IntegrationServer/instances/default/config/security/openid/salesforce.com.json:  "redirection_endpoint_host" : "ip-172-40-192-143.ad.clouddemo.saggov.local",
-# IntegrationServer/instances/default/config/remote.cnf:    <value name="host">ip-172-40-192-143.ad.clouddemo.saggov.local</value>
-# IntegrationServer/instances/default/config/backup/security/openid/salesforce.com.json:  "redirection_endpoint_host" : "ip-172-40-192-143.ad.clouddemo.saggov.local",
-# IntegrationServer/instances/default/config/backup/remote.cnf:    <value name="host">ip-172-40-192-143.ad.clouddemo.saggov.local</value>
-# InternalDataStore/config/elasticsearch.yml:node.name: ip-172-40-192-1431613691068585
-# InternalDataStore/config/elasticsearch.yml:cluster.initial_master_nodes: ["ip-172-40-192-1431613691068585"]
-# InternalDataStore/config/elasticsearch.state.yml:node.name: ip-172-40-192-1431613691068585
-# InternalDataStore/config/elasticsearch.state.yml:cluster.initial_master_nodes: ["ip-172-40-192-1431613691068585"]
+## reset osgi profile
+cd ${SAG_HOME}/IntegrationServer/instances; ./is_instance.sh delete-osgi-profile -Dinstance.name=${INSTANCE_NAME}; ./is_instance.sh create-osgi-profile -Dinstance.name=${INSTANCE_NAME}
